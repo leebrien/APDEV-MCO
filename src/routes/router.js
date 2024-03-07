fd8a71dbac51ed7d961056afeb4cba317510a805
leebrien <lee_sandoval@dlsu.edu.ph>
2024-03-07 21:16:05 +0800

    revised db data and added appropriate json files

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const User = require('../models/model.users');
 
 const router = express.Router();
 
